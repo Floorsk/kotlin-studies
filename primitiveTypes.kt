f6f73df1e5053a1fc789e820e7c8aa364211b4aa
florskdev <florskdev@gmail.com>
2024-01-17 10:55:37 -0300

    variable and types

---
+++ primitiveTypes.kt
@@ -0,0 +1,18 @@
+fun main() {
+/* You can define the types for each variable */
+/*  var number: Int = 10
+    var l: Long = 100L
+    var d: Double = 3.14
+    var f: Float = 3.14F
+    var b: Boolean = true
+ */
+/* It also has type interface wich means you can
+it auto selects based on the value */
+    var number = 10
+    var l = 100L
+    var d = 3.14
+    var f = 3.14F
+    var b = true
+
+    print("${number}, ${l}, ${d}, ${f}, ${b}")
+}
--- variables.kt
+++ variables.kt
@@ -2,6 +2,12 @@ fun main() {
     var name: String = "Florsk"
     var plans: String = "to become a sucessful developer!"
     println("My name is ${name} and I want ${plans}")
+    /* you can also define a variable with val */
+    /* var = can be changed */
+    /* val = can't be changed */
+    val age = 20
+    name = "Davi"
+    println("${name}")
 }
 
 /* I am currently studing variables and here are some examples
