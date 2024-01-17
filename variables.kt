dbb5626d50d1a1feaa70820e323c64696f939fcf
florskdev <florskdev@gmail.com>
2024-01-17 09:51:39 -0300

    first commit

---
+++ variables.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var name: String = "Florsk"
+    var plans: String = "to become a sucessful developer!"
+    println("My name is ${name} and I want ${plans}")
+}
+
+/* I am currently studing variables and here are some examples
+of how those variables work on kotlin*/
